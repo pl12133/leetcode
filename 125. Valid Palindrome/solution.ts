93f18d5d46e79f8813e697c30a557c2f39a31651
Peter Lenahan <12724311+pl12133@users.noreply.github.com>
2022-10-07 10:14:43 -0400

    Solve for #125. Valid Palindrome.

---
+++ 125. Valid Palindrome/solution.ts
@@ -0,0 +1,11 @@
+function isPalindrome(s: string): boolean {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left++] !== s[right--]) {
+            return false;
+        }
+    }
+    return true;
+};
