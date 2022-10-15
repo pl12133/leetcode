7af38b5f7a1dcf41b1d461620068c481022984ad
Peter Lenahan <12724311+pl12133@users.noreply.github.com>
2022-10-15 11:47:20 -0400

    Faster solution for #3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/solution.ts
+++ 3. Longest Substring Without Repeating Characters/solution.ts
@@ -1,26 +1,24 @@
 function lengthOfLongestSubstring(s: string): number {
-    const hashmap = new Map();
-    let pointer: number = 0;
-        
-    const chars = s.split('');
-    for (let index = 0; index < chars.length; ++index) {
-        const char = chars[index];
-        
-        const currentSubstr: string = hashmap.get(pointer) || '';
-        if (index > 0 && !currentSubstr.includes(char)) {
-            hashmap.set(pointer, `${currentSubstr}${char}`);
-        } else {
-            // Move the index back to the character following the duplicate
-            index = index - currentSubstr.length + currentSubstr.indexOf(char) + 1;
-            pointer = index;
-            hashmap.set(pointer, chars[pointer]);
+    if (!s) { return 0; }
+    let left = 0;
+    let right = left + 1;
+    let substr = s[0];
+    while (right <= s.length) {
+        if (left === right) {
+            right++;
+            continue;
         }
-    }
-    
-    let longest: number = 0;
-    for (let substr of hashmap.values()) {
-        longest = Math.max(longest, substr.length);
-    }
+        if (s.indexOf(s[right - 1], left) !== right - 1) {
+            left++;
+            continue;
+        }
+        const nextSubstr = s.substring(left, right);
 
-    return longest;
+        if (nextSubstr.length > substr.length) {
+            substr = nextSubstr;
+        }
+        right++;
+    }
+        
+    return substr.length;
 };
